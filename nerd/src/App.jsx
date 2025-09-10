55f1f14e747b0f0e74908f4f79a66b29d0a6f588
ScriptKarma <cauabdecol@gmail.com>
2025-09-10 15:34:35 -0300

    app.fix

--- nerd/src/App.jsx
+++ nerd/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -9,9 +8,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+
       </div>
       <h1>Vite + React</h1>
       <div className="card">
