6b0fdb443b35b65d3c8032944ecc2d57a2f36d0f
ScriptKarma <cauabdecol@gmail.com>
2025-09-11 14:10:05 -0300

    nav

--- nerd/src/navbar.jsx
+++ nerd/src/navbar.jsx
@@ -3,13 +3,12 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="bg-gray-900 text-white px-6 py-4 flex justify-between items-center">
-      <h1 className="text-xl font-bold">Meu Site</h1>
-      <div className="flex gap-6">
-        <Link to="/" className="hover:text-blue-400 transition">
+    <nav>
+      <div >
+        <Link to="/">
           Home
         </Link>
-        <Link to="/perfil" className="hover:text-blue-400 transition">
+        <Link to="/perfil">
           Perfil
         </Link>
       </div>
