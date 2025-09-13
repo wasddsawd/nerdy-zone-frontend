6e97916ade5532f396b80fe1afedea77a9d7faa2
JazzyVain <renuxlan@gmail.com>
2025-09-12 22:28:02 -0300

    Tentando fazer animações funcionar

--- nerd/src/main.jsx
+++ nerd/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import {AnimatePresence} from "framer-motion";
 import Home from "./Home";
 import Perfil from "./perfil";
 import Eventos from "./paginaEventos";
@@ -8,9 +9,27 @@ import Login from "./pages/login";
 import Cadastro from "./pages/cadastro";
 import "./styles/Geral.css";
 
+
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <div className="routes-wrapper">
+      <AnimatePresence mode="sync">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/login" element={<Login key="login" />} />
+          <Route path="/cadastro" element={<Cadastro key="cadastro" />} />
+        </Routes>
+      </AnimatePresence>
+    </div>
+  );
+}
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
+      <AnimatedRoutes />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/perfil" element={<Perfil />} />
