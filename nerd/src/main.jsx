6c4b6367dd5cc4c9d5879953b38dc5c423ce9325
JazyVain <renxuslan@gmail.com>
2025-09-15 17:36:26 -0300

    gypbuaegth~peqk~yjheq~tohiae]ethheah

--- nerd/src/main.jsx
+++ nerd/src/main.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import {AnimatePresence} from "framer-motion";
 import Home from "./Home";
 import Perfil from "./perfil";
 import Eventos from "./paginaEventos";
-import Login from "./pages/login";
-import Cadastro from "./pages/cadastro";
+import { AnimatedRoutes } from "./pages/AnimatedRoutes"; // seu arquivo com AnimatePresence
 import "./styles/Geral.css";
-
-
-
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -19,9 +14,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/" element={<Home />} />
         <Route path="/perfil" element={<Perfil />} />
         <Route path="/paginaEventos" element={<Eventos />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/cadastro" element={<Cadastro />} />
+        {/* aqui não coloco mais login/cadastro */}
       </Routes>
+      <AnimatedRoutes />
     </Router>
   </React.StrictMode>
 );
