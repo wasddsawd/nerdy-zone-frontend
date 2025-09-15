e82832551e100f5fcac08f31613ec06356003578
wasddsawd <rafaelkrugervidaldasilva@gmail.com>
2025-09-15 14:44:12 -0300

    add navbar.css

--- nerd/src/components/ui/navbar.jsx
+++ nerd/src/components/ui/navbar.jsx
@@ -1,20 +1,15 @@
 import * as React from "react";
 import "../../styles/Geral.css";
+import "../../styles/navbar.module.css"
 import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav>
-      <div >
-        <Link to="/">
-          Home
-        </Link>
-        <Link to="/perfil">
-          Perfil
-        </Link>
-        <Link to="/login">
-        Login</Link>
+      <div className="dentro">
+        <Link to="/" className="">Home</Link>
+        <Link to="/perfil">Perfil</Link>
+        <Link to="/login">Login</Link>
       </div>
     </nav>
   );
 }
- 
