baaf79e7242f63cbb61aa9b6a0a8a40dcfdfa082
ScriptKarma <cauabdecol@gmail.com>
2025-09-15 13:36:57 -0300

    ..

--- nerd/src/navbar.jsx
+++ nerd/src/navbar.jsx
@@ -8,9 +8,6 @@ export default function Navbar() {
         <Link to="/">
           Home
         </Link>
-        <Link to="/perfil">
-          Perfil
-        </Link>
         <Link to="/login">
           Login
         </Link>
