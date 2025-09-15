7c083c48457e1dcb5038bd47d6d4879550807135
wasddsawd <rafaelkrugervidaldasilva@gmail.com>
2025-09-15 16:08:13 -0300

    fix navbar

--- nerd/src/components/ui/navbar.jsx
+++ nerd/src/components/ui/navbar.jsx
@@ -1,14 +1,15 @@
 import * as React from "react";
 import "../../styles/Geral.css";
-import "../../styles/navbar.module.css"
+import styles from "../../styles/navbar.module.css";
 import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
-    <nav>
-      <div className="dentro">
+    <nav className={styles.fora}>
+      <div className={styles.dentro}>
         <Link to="/" className="">Home</Link>
         <Link to="/perfil">Perfil</Link>
         <Link to="/login">Login</Link>
+        <Link to="/paginaEventos">Eventos</Link>
       </div>
     </nav>
   );
