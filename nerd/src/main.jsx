d786f70abf163bf3b19437e072853eea7858202f
wasddsawd <rafaelkrugervidaldasilva@gmail.com>
2025-09-17 17:26:45 -0300

    fix eventospage caminho

--- nerd/src/main.jsx
+++ nerd/src/main.jsx
@@ -18,7 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/perfil" element={<Perfil />} />
-    <Route path="/paginaEventos" element={<Eventos />} />
+    <Route path="/EventoPage" element={<Eventos />} />
     {/* 👉 agrupa login/cadastro aqui dentro */}
     <Route path="/*" element={<AnimatedRoutes />} />
   </Routes>
