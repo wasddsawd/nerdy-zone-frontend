e8d833ef05f77c33ddc43e4b865dd76f3d11ecb6
JazyVain <renxuslan@gmail.com>
2025-09-22 15:15:10 -0300

    Commitando essa bagaça

--- nerd/src/main.jsx
+++ nerd/src/main.jsx
@@ -9,6 +9,7 @@ import "./styles/Geral.css";
 import Venda from "./pages/venda";
 import Navbar from "./components/ui/navbar";
 import { Toaster } from "./components/ui/sonner";
+import Footer from "./components/ui/footer"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
