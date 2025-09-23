04ab7c0c5220b085e005edd2067951796e2f319d
JazyVain <renxuslan@gmail.com>
2025-09-23 15:42:27 -0300

    sojfughadoifhgbdfilhs\fbishdbflhkzsdfbslgjihrbgoiawrghbpwirughbw(riG

--- nerd/src/pages/EventosPage.jsx
+++ nerd/src/pages/EventosPage.jsx
@@ -13,7 +13,7 @@ export default function EventsPage() {
         {/* Sidebar */}
         <aside className={styles.events_sidebar}>
           <section className={styles.filters}>
-            <h2>Filtros</h2>
+            <h1>Filtros</h1>
             <label><input type="checkbox" /> Anime</label>
             <label><input type="checkbox" /> Games</label>
             <label><input type="checkbox" /> HQs</label>
@@ -21,15 +21,14 @@ export default function EventsPage() {
             <label><input type="checkbox" /> Tecnologia</label>
           
           </section>
-
+          {/*
           <section className={styles.calendar}>
-            <h2>Calendário</h2>
-            {/* calendário futuro */}
+            <h1>Calendário</h1>
             <div className={styles.calendar_placeholder}>[Calendário]</div>
           </section>
 
           <section className={styles.featured}>
-            <h2>Destaques</h2>
+            <h1>Destaques</h1>
             <ul>
               <li>Evento 1</li>
               <li>Evento 2</li>
@@ -37,12 +36,12 @@ export default function EventsPage() {
           </section>
 
           <section className={styles.gallery}>
-            <h2>Galeria</h2>
+            <h1>Galeria</h1>
             <div className={styles.gallery_placeholder}>[Imagens]</div>
           </section>
 
           <section className={styles.comments}>
-            <h2>Comentários</h2>
+            <h1>Comentários</h1>
             <div className={styles.comments_placeholder}>[Comentários]</div>
           </section>
           <button className={styles.btncriar}>Criar Eventos</button>
@@ -50,7 +49,7 @@ export default function EventsPage() {
 
       <div className={styles.container}>
         <section className={styles.countdown}>
-            <h2>Próximo Evento em:</h2>
+            <h1>Próximo Evento em:</h1>
             <div className={styles.countdown_timer}>00d 00h 00m 00s</div>
           </section>
 
@@ -58,7 +57,7 @@ export default function EventsPage() {
         <main className={styles.events_main}>
 
           <section className={styles.upcoming_events}>
-            <h2>Eventos Futuros</h2>
+            <h1>Eventos Futuros</h1>
             <div className={styles.event_card}>
               <h3>Nome do Evento</h3>
               <button>Inscrever-se</button>
@@ -66,7 +65,7 @@ export default function EventsPage() {
           </section>
 
           <section className={styles.event_details}>
-            <h2>Detalhes</h2>
+            <h1>Detalhes</h1>
             <div className={styles.map_placeholder}>[Mapa]</div>
             <div className={styles.guests_placeholder}>[Convidados]</div>
             <div className={styles.competitions_placeholder}>[Competições]</div>
